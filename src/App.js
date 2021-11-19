d1c57c79f5787c9af3c2636176f8d721351878b2
HaydenCrocker <crocker.hayden@gmail.com>
2021-11-19 10:46:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import PieceOfArt from './PieceOfArt';
+// on page load, make a call to the API 
+// Store that data from the APi in state 
+// display the results to the page 
+// -> image and art piece information 
 
 function App() {
+  // piece of state to hold our art pieces 
+  const [art, setArt] = useState([])
+
+  // when the component mounts, call our API
+  useEffect(() => {
+    // in here we will all our API using axios 
+    const apiKey = 'bypZuosH';
+    axios({
+      url: 'https://www.rijksmuseum.nl/api/en/collection',
+      method: 'GET',
+      responseType: 'json',
+      params: {
+        key: apiKey,
+        imgonly: true
+      }
+    })
+      .then(response => {
+        console.log(response.data.artObjects)
+        setArt(response.data.artObjects)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Look at our Art!</h1>
+      {
+        art.map(artwork => {
+          return (
+            <PieceOfArt
+              key={artwork.id}
+              alt={artwork.title}
+              title={artwork.longTitle}
+              imagePath={artwork.webImage.url}
+            />
+          )
+        })
+      }
     </div>
   );
 }
